4a42cd22ccf0bbf1d8ded797513224a37eccdd14
Brian <briankonji00@gmail.com>
2024-08-25 11:24:20 +0200

    Number 8:implementing the classes

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of the Bank class
+        Bank myBankAccount = new Bank("BTK", 230087);
+        }
+}
