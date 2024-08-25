2e49c48fbae8f68b2bc56caf2da501ef0029252f
Brian <briankonji00@gmail.com>
2024-08-25 10:30:19 +0200

    Number 2 : adding method to deposit money

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,10 @@ public class Bank {
     // Fields for account type and account balance
     private String accountType;
     private double accountBalance;
+
+    // Method to deposit money
+    public double deposit(double amount) {
+        accountBalance += amount;
+        return accountBalance;
+    }
 }
