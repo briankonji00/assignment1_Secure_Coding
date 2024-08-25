7a1a547e8d5a63f97ae6a62f91858a8a11e3739b
Brian <briankonji00@gmail.com>
2024-08-25 11:15:39 +0200

    Number 6:Creating a class Insurance that inherits from the Bank class.

---
+++ src/Insurance.java
@@ -0,0 +1,8 @@
+public class Insurance extends Bank {
+    // Constructor for Insurance class
+    public Insurance(String accountType, double accountBalance) {
+        super(accountType, accountBalance); // Call the constructor of the superclass (Bank)
+
+    }
+}
+
